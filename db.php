7328f41e9cb84cc3dedd1825efe142dbd0566cc1
root <root@ip-172-31-53-149.us-west-2.compute.internal>
2020-12-21 06:00:27 +0000

    adding my db, execute,log in and more file

---
+++ db.php
@@ -0,0 +1,7 @@
+<?php 
+$mysql_hostname = "localhost";
+$mysql_user = "username";
+$mysql_password = "password";
+$mysql_database = "database";
+$con = mysqli_connect($mysql_hostname, $mysql_user, $mysql_password, $mysql_database);
+?>
